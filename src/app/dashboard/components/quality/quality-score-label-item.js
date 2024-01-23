237d118c875250e58dbe031988f0dbe5d2303223
Wiktor <62408118+vizi-rgb@users.noreply.github.com>
2024-01-23 11:04:46 +0200

    Added worst aspects widget (#7)

---
+++ src/app/dashboard/components/quality/quality-score-label-item.js
@@ -0,0 +1,23 @@
+function QualityScore({ quality }) {
+  return (
+    <div>
+      <div className="w-[50px] flex flex-col border-2 border-[#9381FF] rounded-[10px] items-center overflow-hidden">
+        <div className="h-[30px] flex flex-row justify-center items-center">
+          <p className="rating-text text-[#9381FF]">{quality.score}</p>
+        </div>
+        <div className="h-[30px] w-full flex flex-row justify-center items-center bg-[#9381FF]">
+          <p className="rating-text text-white">{quality.max}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function QualityScoreAndLabelItem({ quality, label }) {
+  return (
+    <div className="flex flex-row items-center gap-x-2.5">
+      <p className="text-black text-right max-w-[80px]">{label}</p>
+      <QualityScore quality={quality} />
+    </div>
+  );
+}
